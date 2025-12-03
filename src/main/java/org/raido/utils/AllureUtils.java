5c579806db6f3153c5e18f018328ac34423bda93
Sergey <sergeybazhaykin@gmail.com>
2025-12-04 00:45:04 +0400

    добавление Allure

---
+++ src/main/java/org/raido/utils/AllureUtils.java
@@ -0,0 +1,16 @@
+package org.raido.utils;
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class AllureUtils {
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public static byte[] takeScreenshot(WebDriver driver) {
+        if (driver == null) {
+            return new byte[0];
+        }
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+}
