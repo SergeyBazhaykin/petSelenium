4ed3a1d044654ec585870660e70d84f02c395582
Sergey <sergeybazhaykin@gmail.com>
2025-12-03 18:33:56 +0400

    вынесена логика страницы из класса теста

--- src/main/java/org/raido/ExchangeRatesPage.java
+++ src/main/java/org/raido/ExchangeRatesPage.java
@@ -18,11 +18,45 @@ public class ExchangeRatesPage {
         this.waitTimeInSeconds = 5;
     }
 
-    public List<WebElement> getExchangeRatesList() {
-        return driver.findElements(exchangeCardLocator);
+    public List<ExchangeRateCard> getExchangeRateCards() {
+        WaitUtils.waitForElementVisibility(driver, exchangeCardLocator, waitTimeInSeconds);
+
+        List<WebElement> cardElements = driver.findElements(exchangeCardLocator);
+
+        return cardElements.stream()
+                .map(ExchangeRateCard::new) // Создаем объект внутреннего класса
+                .collect(Collectors.toList());
     }
 
     public String getCurrentUrl() {
         return driver.getCurrentUrl();
+    }
+
+    public static class ExchangeRateCard {
+
+        private final WebElement cardRootElement;
+
+        private static final By NAME_LOCATOR =
+                By.xpath("./div[1]/div[2]/div[@class='exchange-card__country__name']");
+        private static final By BUY_LOCATOR = By.xpath("./div[2]");
+        private static final By SALE_LOCATOR = By.xpath("./div[3]");
+
+        public ExchangeRateCard(WebElement cardRootElement) {
+            this.cardRootElement = cardRootElement;
+        }
+
+        public String getCurrencyName() {
+            return cardRootElement.findElement(NAME_LOCATOR).getText().trim();
+        }
+
+        public double getBuyValue() {
+            String text = cardRootElement.findElement(BUY_LOCATOR).getText().trim();
+            return Double.parseDouble(text);
+        }
+
+        public double getSaleValue() {
+            String text = cardRootElement.findElement(SALE_LOCATOR).getText().trim();
+            return Double.parseDouble(text);
+        }
     }
 }
